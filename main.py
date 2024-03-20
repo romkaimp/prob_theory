8d0b3320d942c8d9a8980a0a72e6ae3cf771d9b6
romkaimp <roman.150704@mail.ru>
2024-03-20 17:14:06 +0300

    .gitignore

--- main.py
+++ main.py
@@ -4,6 +4,83 @@ from math import pi, exp
 from numpy import array
 
 
+
+class Functions():
+    n = None
+    m = None
+    p = None
+
+    def __init__(self, n: int, m: int, p: float = 0):
+        self.n = n
+        self.m = m
+        self.p = p
+
+
+    class CashSolution():
+        cash: list = []
+        f = Relations()
+
+        class Relations():
+            def __init__(self):
+                pass
+
+            def
+
+        def counting(expression: str):
+
+
+
+    def factorial(self, n: int=-1):
+        if self.n == None:
+            if n != -1:
+                self.n = n
+        if n != None:
+        k = 1
+        for i in range(1, n + 1):
+            k *= i
+        return k
+
+    def p_(n):
+        return factorial(n)
+
+    def p_r(n, *args):
+        j = [factorial(x) for x in args]
+        multiply_res = reduce((lambda x, y: x * y), j)
+        return factorial(n) / multiply_res
+
+    def a_(n, m):
+        return factorial(n) / factorial(n - m)
+
+    def a_r(n, m):
+        return pow(n, m)
+
+    def c_(n, m):
+        return a_(n, m) / factorial(m)
+
+    def c_r(n, m):
+        res = c_(m + n - 1, m - 1)
+        if res < 1:
+            return 1
+        return res
+
+    def bernulli(n, k, q1, q2: float = 0):
+        q2 = 1 - q1
+        return c_(n, k) * pow(q1, k) * pow(q2, n - k)
+
+    def muavr_laplace(n, m, p, pi=pi):
+        q = 1 - p
+        return (1 / pow(2 * pi * n * p * q, 1 / 2)) * exp(-(pow(m - n * p, 2)) / (2 * n * p * q))
+
+    def muavr_integral(n, k1, k2, p, pi=pi):
+        q = 1 - p
+
+        def integrand(x):
+            return exp(-pow(x, 2) / 2)
+
+        return 1 / (pow(2 * pi, 1 / 2)) * quad(integrand, (k1 - n * p) / pow(n * p * q, 1 / 2),
+                                               (k2 - n * p) / pow(n * p * q, 1 / 2))
+
+
 def factorial(n):
     k = 1
     for i in range(1, n + 1):
@@ -11,7 +88,7 @@ def factorial(n):
     return k
 
 
-def p(n):
+def p_(n):
     return factorial(n)
 
 
@@ -21,7 +98,7 @@ def p_r(n, *args):
     return factorial(n)/multiply_res
 
 
-def a(n, m):
+def a_(n, m):
     return factorial(n)/factorial(n-m)
 
 
@@ -29,24 +106,20 @@ def a_r(n, m):
     return pow(n, m)
 
 
-def c(n, m):
-    return a(n, m)/factorial(m)
+def c_(n, m):
+    return a_(n, m)/factorial(m)
 
 
 def c_r(n, m):
-    res = c(m + n - 1, m - 1)
+    res = c_(m + n - 1, m - 1)
     if res < 1:
         return 1
     return res
 
 
-def n_na_m(n, m):
-    summa = 0
-
-
 def bernulli(n, k, q1, q2: float = 0):
     q2 = 1 - q1
-    return c(n, k)*pow(q1, k)*pow(q2, n-k)
+    return c_(n, k)*pow(q1, k)*pow(q2, n-k)
 
 
 def muavr_laplace(n, m, p, pi=pi):
