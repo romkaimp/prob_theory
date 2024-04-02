8cc1ade269616471c64c7f0897906c1d56311d6d
romkaimp <roman.150704@mail.ru>
2024-04-02 13:26:07 +0300

    муавра-лапласа (5 задача)

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-from fastapi import FastAPI
-
-app = FastAPI()
-
-
-@app.get("/items/{item_id}")
-async def read_item(item_id):
-    return {"item_id": item_id}
+name = "VLAD"
+verb = "UMER"
+for i in range(100):
+    print(name + verb
+          )
